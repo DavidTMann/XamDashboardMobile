1ee4dd026a5bf4b04abc61edc09fd71267637753
DavidTMann <d.mann@cgi.com>
2016-06-06 12:02:07 +0100

    Removed projects and created new blank android project

--- XamarinDashboardApp/XamarinDashboardApp/App.cs
+++ XamarinDashboardApp/XamarinDashboardApp/App.cs
@@ -12,7 +12,19 @@ namespace XamarinDashboardApp
         public App()
         {
             // The root page of your application
-            MainPage = new HomePage();
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            XAlign = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
         }
 
         protected override void OnStart()
